e8ccac124762c1297294cebd0ccc0597273c97df
Pedro H <pedro.26.3.h@gmail.com>
2023-08-03 22:17:36 -0300

    Adicionar biblioteca utilidades

---
+++ util.h
@@ -0,0 +1,34 @@
+/**
+* @file     util.h
+* @brief    Biblioteca com funções auxiliares
+* @author   Pedro Henrique
+* @date     2023-01-26
+*/
+
+#pragma once
+#ifndef __UTIL_h__
+#define __UTIL_h__
+
+#include <stdio.h>
+#include <string.h>
+#include "util.h"
+
+
+// Tipos
+
+// Funcoes
+void imprimirCadeiaDeCaracter(char caracter, int N) {
+    for (int i = 0; i < N; i++) {
+        printf("%c", caracter);
+    }
+}
+
+void removeQuebraLinhaFinal(char dados[]) {
+    int tamanho = (int) strlen(dados);
+
+    if (tamanho && dados[tamanho - 1] == '\n') {
+        dados[tamanho - 1] = '\0';
+    }
+}
+
+#endif
